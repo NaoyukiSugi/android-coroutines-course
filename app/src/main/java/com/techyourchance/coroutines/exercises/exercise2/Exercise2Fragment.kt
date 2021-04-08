ed8541876a578da450b83d34e99fd29cdaae9310
Naoyuki Sugi <bnsgny@gmail.com>
2021-04-08 13:44:25 +0900

    solve

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise2/Exercise2Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise2/Exercise2Fragment.kt
@@ -13,10 +13,7 @@ import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.exercises.exercise1.GetReputationEndpoint
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class Exercise2Fragment : BaseFragment() {
 
@@ -29,6 +26,8 @@ class Exercise2Fragment : BaseFragment() {
 
     private lateinit var getReputationEndpoint: GetReputationEndpoint
 
+    private var job: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getReputationEndpoint = compositionRoot.getReputationEndpoint
@@ -39,7 +38,7 @@ class Exercise2Fragment : BaseFragment() {
 
         edtUserId = view.findViewById(R.id.edt_user_id)
         edtUserId.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 btnGetReputation.isEnabled = !s.isNullOrEmpty()
@@ -51,7 +50,7 @@ class Exercise2Fragment : BaseFragment() {
         btnGetReputation = view.findViewById(R.id.btn_get_reputation)
         btnGetReputation.setOnClickListener {
             logThreadInfo("button callback")
-            coroutineScope.launch {
+            job = coroutineScope.launch {
                 btnGetReputation.isEnabled = false
                 val reputation = getReputationForUser(edtUserId.text.toString())
                 Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
@@ -60,6 +59,12 @@ class Exercise2Fragment : BaseFragment() {
         }
 
         return view
+    }
+
+    override fun onStop() {
+        super.onStop()
+        job?.cancel()
+        btnGetReputation.isEnabled = true
     }
 
     private suspend fun getReputationForUser(userId: String): Int {
